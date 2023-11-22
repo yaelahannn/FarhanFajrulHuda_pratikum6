5d8d91558b74b91404ea386d7e6009e0b994fc62
yaelahannn <125876464+yaelahannn@users.noreply.github.com>
2023-11-22 12:31:28 +0700

    Add files via upload

---
+++ praktikum_modul6-main/lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Praktikum Navigasi',
+      home: HalamanBeranda(),
+    );
+  }
+}
+
+class HalamanBeranda extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Praktikum Navigasi'),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          // Penting: Hilangkan semua padding dari ListView
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Image.asset('assets/images/telkomuniversity.png'),
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.library_books),
+              title: Text('Lihat Koleksi'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HalamanJelajah()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.man_3),
+              title: Text('Akun'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HalamanAkun()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Ini adalah tampilan Halaman Beranda'),
+      ),
+    );
+  }
+}
+
+class HalamanJelajah extends StatefulWidget {
+  @override
+  _HalamanJelajahState createState() => _HalamanJelajahState();
+}
+
+class _HalamanJelajahState extends State<HalamanJelajah> {
+  int _selectedIndex = 0;
+  final List<Widget> _widgetOptions = <Widget>[
+    const GridViewWidget('Formal'),
+    const GridViewWidget('Kasual'),
+    const GridViewWidget('Vintage'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Koleksi'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Formal',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.beach_access),
+            label: 'Kasual',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ac_unit),
+            label: 'Vintage',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class GridViewWidget extends StatelessWidget {
+  final String category;
+
+  // Marking the constructor as const
+  const GridViewWidget(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    // Build your widget here
+    return Text('GridView for $category');
+  }
+}
+
+class HalamanAkun extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Akun'),
+      ),
+      body: Center(
+        child: Text('Ini adalah tampilan Halaman Akun'),
+      ),
+    );
+  }
+}
